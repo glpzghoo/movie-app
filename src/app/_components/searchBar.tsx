027c8183896d84f2f5fee7c8e33b6c7f2d67b2fa
adiyakhuu1 <my.as.glpzghoo@gmail.com>
2024-12-26 22:32:46 +0800

    testing thing

--- src/app/_components/searchBar.tsx
+++ src/app/_components/searchBar.tsx
@@ -2,6 +2,7 @@
 import { useEffect, useState } from "react";
 import { Movie, options } from "../page";
 import { Link } from "lucide-react";
+import { FaArrowRight } from "react-icons/fa";
 type target = {
   value: string;
 };
@@ -33,11 +34,12 @@ type theMovie = {
   vote_count: number;
 };
 export const SearchBar = () => {
-  const [search, setSearch] = useState("sonic");
+  const [search, setSearch] = useState("");
   const [data, setData] = useState<data>();
   const input = (e: Event) => {
     const value: string = e.target.value;
-    console.log("input", e.target.value);
+    console.log("input", e);
+    // if()
     setSearch(value);
   };
   useEffect(() => {
@@ -51,40 +53,54 @@ export const SearchBar = () => {
       console.log("searching data", data);
     };
     fetchData();
-  }, []);
+  }, [search]);
   //   const results = data.results;
   console.log("checking the damn data", data);
 
   const ResultDiv = (props: Props) => {
-    const theMovie = props.results[0];
+    const theMovies = props.results;
     return (
-      <>
-        {data && (
-          <a className="bg-green-400 w-[335px]" href={`./movie/`}>
-            <div>
-              <img
-                className="w-16 h-25"
-                src={`https://image.tmdb.org/t/p/original${theMovie.poster_path}`}
-              />
-            </div>
-            <div>
-              <h1 className="">{theMovie.title}</h1>
-            </div>
-          </a>
-        )}
-      </>
+      <div className="border-border overflow-hidden rounded-xl p-3 shadow-2xl flex flex-col bg-white absolute right-4 gap-1">
+        {data &&
+          theMovies
+            .map((theMovie) => (
+              <a
+                className=" w-[335px] p-2 relative flex border-b-2 border-gray-200"
+                href={`/movie/${theMovie.id}`}
+              >
+                <div>
+                  <img
+                    className="w-16 h-25"
+                    src={`https://image.tmdb.org/t/p/original${theMovie.poster_path}`}
+                  />
+                </div>
+                <div className="flex flex-col justify-evenly ">
+                  <h1 className="">{theMovie.title}</h1>
+                  <div className="flex">
+                    <img className="w-5" src="/img/rating.svg" />
+                    <div>{Math.floor(theMovie.vote_average * 10) / 10}/10</div>
+                  </div>
+                  <div>{theMovie.release_date}</div>
+                  <div className="absolute bottom-0 flex items-center gap-2  right-0">
+                    See more <FaArrowRight />
+                  </div>
+                </div>
+              </a>
+            ))
+            .slice(1, 5)}
+        {search && <div>See all results for "{search}"</div>}
+      </div>
     );
   };
   return (
     <>
       {data && (
         <div>
-          <input
-            onChange={input}
-            placeholder="Search"
-            className="hidden w-1/2 sm:block"
-          />
-          <ResultDiv results={data} />
+          <div className="relative hidden w-full sm:block">
+            <input onChange={input} placeholder="Search" className="" />
+            <ResultDiv results={data} />
+          </div>
+
           <button className="block sm:hidden">
             <img className="w-9" src="/img/search.png" />
           </button>
